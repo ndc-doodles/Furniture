ae2d0f83eb3df9c223952a90a98a992a692edb93
N-meapp <developersnme@gmail.com>
2025-08-12 15:51:49 +0530

    updated

--- static/js/admin_scripts.js
+++ static/js/admin_scripts.js
@@ -22,3 +22,34 @@
         // TODO: Integrate with backend (AJAX or Django view) for actual delete
       }
     }
+
+
+
+
+
+
+
+
+
+ 
+  let currentEditRow = null;
+
+  function openEditModal(row) {
+    currentEditRow = row;
+    const categoryName = row.querySelector('.category-name').innerText;
+    document.getElementById('editCategoryName').value = categoryName;
+    document.getElementById('editModal').classList.remove('hidden');
+  }
+
+  function closeEditModal() {
+    document.getElementById('editModal').classList.add('hidden');
+  }
+
+  document.getElementById('editForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const updatedName = document.getElementById('editCategoryName').value;
+    if (currentEditRow) {
+      currentEditRow.querySelector('.category-name').innerText = updatedName;
+    }
+    closeEditModal();
+  });
